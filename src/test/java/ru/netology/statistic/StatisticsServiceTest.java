8d9a100caba44c18f8c940c5e4b9915034d803fe
Alexei Babeshko <alexbabeshko08@gmail.com>
2024-03-03 13:46:08 +0300

    StatisticsServiceTest.java changed

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -6,13 +6,13 @@ import org.junit.jupiter.api.Test;
 public class StatisticsServiceTest {
 
     @Test
-    void findMax() {
+    void findMaxEmptyArray() {
         StatisticsService service = new StatisticsService();
 
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 12;
+        long[] incomes = {-12, -5, -8, -4, -5, -3, -8, -6, -11, -11, -12};
+        long expected = -3;
 
-        long actual = service.findMax(incomesInBillions);
+        long actual = service.findMax(incomes);
 
         Assertions.assertEquals(expected, actual);
     }
